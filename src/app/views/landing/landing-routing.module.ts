bb0a395a4c2431b90e7a552dd467551a1c9fb04e
Mark Gil Baterna <mark.baterna@cloudqwest.net>
2022-03-12 09:54:31 +0800

    about page

--- src/app/views/landing/landing-routing.module.ts
+++ src/app/views/landing/landing-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";  
+import { AboutComponent } from "./about.component";
 import { LandingComponent } from "./landing.component";
 
 export const routes: Routes = [
@@ -7,6 +8,10 @@ export const routes: Routes = [
   {
     path: '',
     component: LandingComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
   },
 
 ]
--- src/app/views/landing/landing.module.ts
+++ src/app/views/landing/landing.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from "@angular/core";
 import { LandingRoutingModule } from "./landing-routing.module";
 import { LandingComponent } from "./landing.component";
 import { MatCarouselModule } from '@ngmodule/material-carousel';
+import { AboutComponent } from "./about.component";
 
 
 
 @NgModule({
     declarations: [
       LandingComponent,
+      AboutComponent
     ],
     imports: [
       CommonModule,
